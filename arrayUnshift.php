49f5300bbfdf458dfe8e5f944121d42fc22be05b
Arun Bastola <cixwarearun@gmail.com>
2020-03-04 14:12:59 +0545

    adding value at the start of the array

---
+++ arrayUnshift.php
@@ -0,0 +1,9 @@
+<?php
+//pushing the value at the start of the array
+   $array= [1,33,44,43];
+   print_r($array);
+   echo '<br>';
+   echo '<br>';
+   array_unshift($array,22,29);
+   print_r($array);
+?>
