71927b1de5b9ab807e54c02ab60c4c50d698febb
Arun Bastola <cixwarearun@gmail.com>
2020-03-04 14:20:09 +0545

    Adding and replacing the value in array

---
+++ addingAndReplacingValue.php
@@ -0,0 +1,14 @@
+<?php
+  $array=[1,3,6];
+  print_r($array);
+  echo '<br>';
+  echo '<br>';
+  $array[20]=33;
+  //adding value to the array
+  print_r($array);
+  echo '<br>';
+  echo '<br>';
+  $array[2]=100;
+  //replacing value of the array
+    print_r($array);
+?>
