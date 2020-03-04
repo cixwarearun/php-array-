539c76938a275b766fee55957c585acc863b6306
Arun Bastola <cixwarearun@gmail.com>
2020-03-04 14:27:43 +0545

    removing index from the array

---
+++ unset.php
@@ -0,0 +1,12 @@
+<?php
+  $animals= ['elephant','leopard','tiger'];
+  //printing the array of animals
+  echo 'Printing the array of animals';
+  print_r($animals);
+  echo '<br>';
+  echo '<br>';
+  //removing index form the array
+  echo 'Removing the idex 1 from the array';
+  unset($animals[1]);
+  print_r ($animals);
+?>
