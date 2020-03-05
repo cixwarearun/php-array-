d72c57db3a043f620afdb7470cc3633805f0010a
Arun Bastola <cixwarearun@gmail.com>
2020-03-05 15:13:40 +0545

    arrray_diff_uassoc

---
+++ array_diff_uassoc.php
@@ -0,0 +1,16 @@
+<?php
+function myfunction($a,$b)
+{
+if ($a===$b)
+  {
+  return 0;
+  }
+  return ($a>$b)?1:-1;
+}
+
+$a1=array("a"=>"red","b"=>"green","c"=>"blue");
+$a2=array("a"=>"red","b"=>"green","e"=>"blue");
+
+$result=array_diff_uassoc($a2,$a1,"myfunction");
+print_r($result);
+?>
