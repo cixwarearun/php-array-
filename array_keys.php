9a50cc29271b3c66609be67a371cb65a632b36ca
Arun Bastola <cixwarearun@gmail.com>
2020-03-06 11:58:45 +0545

    array_keys:: prints the index or key value of the array

---
+++ array_keys.php
@@ -0,0 +1,8 @@
+<?php
+//returns the index value of the index array
+print_r(array_keys(['Arun','Bibek','Diken','Anita','Binod','Sibu']));
+echo '<br>';
+echo '<br>';
+//returns the key value of the associative arrays
+print_r(array_keys(['arun'=>'arun','bibek'=>'bibek']));
+?>
