8431040f719a541015e42719c3941ca8eb8c82b4
Arun Bastola <cixwarearun@gmail.com>
2020-03-07 10:19:35 +0545

    array_pop:: delete the last element of the array

---
+++ array_pop.php
@@ -0,0 +1,8 @@
+<?php
+//The array_pop() function deletes the last element of an array.
+  $a=['arun','barun','maka','bibek','anita'];
+  array_pop($a);
+  print_r($a);
+
+  //doesnot support print_r(array_pop($a));
+ ?>
