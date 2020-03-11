2676139a6580863dd78da6d15df00436b0d3d3e5
Arun Bastola <cixwarearun@gmail.com>
2020-03-11 15:38:03 +0545

    array_values: returns an array containing all the values of an array

---
+++ array_values.php
@@ -0,0 +1,6 @@
+<?php
+//The array_values() function returns an array containing all the values of an array
+$c=['a'=>'arun','b'=>'sibu','c'=>'arun','d'=>'binod'];
+$b=array_values($c);    
+print_r($b);
+?>
